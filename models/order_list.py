d599fb3c78f5cbcd08756fe9d02afaea31fabe86
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-01-25 17:15:32 +0000

    added content on individual order page

--- models/order_list.py
+++ models/order_list.py
@@ -1,6 +1,6 @@
 from models.order import *
 
 order1 = Order("Outlies", "Lucy Watts", "Sophie Moore", "13/01/2023", 1, 1)
-order2 = Order("The House", "Simon Lelic", "Jane Bennett", "27/01/2023", 1, 2)
+order2 = Order("The House", "Simon Lelic", "Iris Robertson", "27/01/2023", 1, 2)
 
 orders = [order1, order2]
